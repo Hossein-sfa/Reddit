cb35f520147b7e9ac64651ab5b2e88d01d558a2d
Navid <navidraeiszadeh@gmail.com>
2022-05-26 02:13:39 +0430

    post & addpost prt1

--- lib/addPost.dart
+++ lib/addPost.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'Post.dart';
 
 class AddPost extends StatefulWidget {
