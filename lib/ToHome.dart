7c65cdd9e5a7dd4eebdcdee6d1590d7fbcf6d50f
Navid <navidraeiszadeh@gmail.com>
2022-06-04 11:52:33 +0430

    Fix & update To home

--- lib/ToHome.dart
+++ lib/ToHome.dart
@@ -1,36 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:reddit/user%20detail.dart';
+import 'package:reddit/userSetting.dart';
+import 'Post.dart';
 import 'SearchPage.dart';
+import 'Setting.dart';
+import 'addCommunitie.dart';
 import 'addPost.dart';
-import 'posts.dart';
-import 'feed.dart';
 
 class ToHome extends StatefulWidget {
   const ToHome({Key? key}) : super(key: key);
 
   @override
-  State<ToHome> createState() => HomeState();
+  State<ToHome> createState() => homeState();
 }
 
-class HomeState extends State<ToHome> {
-  int _selectedIndex = 0;
+class homeState extends State<ToHome> {
+  List<Post> tasksList = [];  //this is a temp list!!
 
-  num length = UserPosts.posts.length;
+  void addTask(Post post) {
+    setState(() {
+      tasksList.add(post); //ToDo => shamsi date & each post of per associations
+     // sort posts by time
+    });
+  }
+
+  void deleteTask(Post post) {
+    setState(() {
+      //ToDo => For delete post
+      tasksList.remove(Post);
+    });
+  }
+
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        backgroundColor: const Color(0xB3FFFFFF), // gray
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black45,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/profiledesign.jfif'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Menu',
+                style: (TextStyle(fontSize: 20, color: Colors.deepOrangeAccent)),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            CircleAvatar(
+              radius: 30,
+              foregroundImage: const AssetImage('assets/images/circleAvatar.png'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) {
+                        return const userDetail();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text("Change user profile"),
+              trailing: const Icon(Icons.change_circle , color: Colors.blueAccent),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) {
+                      return const UserSetting(key: Key('userSetting'));
+                    },
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Most Viewed Community"),
+              trailing: const Icon(Icons.launch),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text("Add Community"),
+              trailing: const Icon(Icons.add , color: Colors.green),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) {
+                      return CreatProfile(key: UniqueKey());
+                    },
+                  ),
+                );
+              },
+            ),
+            ListTile(
+
+              title: const Text("Settings"),
+              trailing: const Icon(Icons.settings , color: Colors.brown),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) {
+                      return Setting(key: UniqueKey());
+                    },
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Logout"),
+              trailing: const Icon(Icons.power_settings_new , color: Colors.red),
+              onTap: () {
+                //ToDo => Go to login page and delete user data from mobile and send data to server
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: const Color(0X73000000),
         title: const SizedBox(
-          width: double.infinity,
-          height: 40,
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text("Reddit"),
-          ),
-        ),
+            width: double.infinity,
+            height: 40,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Reddit"),
+            )),
         actions: [
+          // Go to Search Page
           IconButton(
               onPressed: () {
                 Navigator.push(
@@ -38,7 +152,7 @@ class HomeState extends State<ToHome> {
                   MaterialPageRoute(
                     builder: (_) {
                       return const SearchPage(
-                        key: Key("navid"),
+                        key: Key("Search page"),
                       );
                     },
                   ),
@@ -48,15 +162,26 @@ class HomeState extends State<ToHome> {
           Align(
               alignment: Alignment.centerRight,
               child: IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.menu_rounded)))
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) {
+                          return const Setting(
+                            key: Key("About us"),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.settings)))
         ],
       ),
-      body: const Feed(),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black54,
         selectedFontSize: 18,
         selectedIconTheme:
-            const IconThemeData(color: Colors.orangeAccent, size: 22),
+        const IconThemeData(color: Colors.orangeAccent, size: 22),
         selectedItemColor: Colors.orangeAccent,
         selectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
         items: const <BottomNavigationBarItem>[
@@ -101,10 +226,10 @@ class HomeState extends State<ToHome> {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) {
-            return const AddPost(
+          builder: (_) {
+            return AddPost(
               //addNewPost: addTask,
-              key: Key("navid"),
+              key: const Key("navid"),
             );
           },
         ),
