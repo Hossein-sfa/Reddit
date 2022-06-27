88b7aee9b7a7ab86aa8db4c0e0ca81e39edbbf9f
Navid <navidraeiszadeh@gmail.com>
2022-06-27 11:32:23 +0430

    change some ui setting and add background

--- lib/siginpage.dart
+++ lib/siginpage.dart
@@ -51,99 +51,110 @@ class SignInState extends State<SignIn> {
     return Material(
       child: Scaffold(
         appBar: AppBars.reddit,
-        body: SafeArea(
-          child: Center(
-            child: Wrap(
-              children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: TextField(
-                        controller: userName,
-                        keyboardType: TextInputType.text,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          filled: true,
-                          labelText: 'UserName: ',
-                          labelStyle: const TextStyle(color: Colors.redAccent),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(100),
-                            borderSide:
-                                const BorderSide(color: Color(0xfffdfdfd)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(100),
-                            borderSide:
-                                const BorderSide(color: Color(0xfffc4040)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
-                      child: TextFormField(
-                        controller: password,
-                        obscureText: obscure,
-                        keyboardType: TextInputType.text,
-                        textAlign: TextAlign.center,
-                        onChanged: (text) {
-                          passwordErrorMessage = isValidPassword(password.text)
-                              ? null
-                              : 'This password is not strong enough.\nIt should have at least one lowercase, uppercase letter and a number.\nand it should be more than 8 characters.';
-                        },
-                        decoration: InputDecoration(
-                          filled: true,
-                          labelText: 'Password: ',
-                          suffixIcon: IconButton(
-                            icon: Icon(obscure
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                            onPressed: () {
-                              setState(() {
-                                obscure = !obscure;
-                              });
-                            },
-                          ),
-                          labelStyle: const TextStyle(color: Colors.redAccent),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(100),
-                            borderSide:
-                                const BorderSide(color: Color(0xfffdfdfd)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(100),
-                            borderSide:
-                                const BorderSide(color: Color(0xfffc4040)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
+        body: Container(
+          constraints: const BoxConstraints.expand(),
+          decoration:  const BoxDecoration(image: DecorationImage(image: AssetImage("assets/images/1.jpg"),fit: BoxFit.cover)),
+          child: SafeArea(
+            child: Center(
+              child: Wrap(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white38,
+                      borderRadius: BorderRadius.all(Radius.circular(18)),),
+                    width: 365,
+                    height: 320,
+                    child: Column(
                       children: [
-                        const SizedBox(width: 25),
-                        const Text("Don't have an account? : "),
-                        TextButton(
-                          child: const Text('Sign up'),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const SignUp(),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15 , top: 20 , bottom: 5),
+                          child: TextField(
+                            controller: userName,
+                            keyboardType: TextInputType.text,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              filled: true,
+                              labelText: 'UserName: ',
+                              labelStyle: const TextStyle(color: Colors.red , fontWeight: FontWeight.bold),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                const BorderSide(color: Color(0xfffdfdfd)),
                               ),
-                            );
-                          },
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                const BorderSide(color: Color(0xfffc4040)),
+                              ),
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      child:
-                          const Text('Sign In', style: TextStyle(fontSize: 20)),
-                      onPressed: userNameChecker && passwordChecker
-                          ? () {
+                        const SizedBox(height: 15),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15, right: 15 ),
+                          child: TextFormField(
+                            controller: password,
+                            obscureText: obscure,
+                            keyboardType: TextInputType.text,
+                            textAlign: TextAlign.center,
+                            onChanged: (text) {
+                              passwordErrorMessage = isValidPassword(password.text)
+                                  ? null
+                                  : 'This password is not strong enough.\nIt should have at least one lowercase, uppercase letter and a number.\nand it should be more than 8 characters.';
+                            },
+                            decoration: InputDecoration(
+                              filled: true,
+                              labelText: 'Password: ',
+                              suffixIcon: IconButton(
+                                icon: Icon(obscure
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    obscure = !obscure;
+                                  });
+                                },
+                              ),
+                              labelStyle: const TextStyle(color: Colors.red , fontWeight: FontWeight.bold),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                const BorderSide(color: Color(0xfffdfdfd)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                                borderSide:
+                                const BorderSide(color: Color(0xfffc4040)),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            const SizedBox(width: 25),
+                            const Text("Don't have an account?  "),
+                            TextButton(
+                              child: const Text('Sign up' , ),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const SignUp(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            child:
+                            const Text('Sign In', style: TextStyle(fontSize: 20)),
+                            onPressed: userNameChecker && passwordChecker
+                                ? () {
                               // ToDo : sending username and password to server in phase to project
                               Navigator.pushReplacement(
                                 context,
@@ -152,21 +163,24 @@ class SignInState extends State<SignIn> {
                                 ),
                               );
                             }
-                          : null,
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
+                                : null,
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              primary: const Color(0xffffb34f),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 60,
+                                vertical: 15,
+                              ),
+                            ),
+                          ),
                         ),
-                        primary: const Color(0xffffb34f),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 120,
-                          vertical: 25,
-                        ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
