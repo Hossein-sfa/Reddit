5fc6fe0934ab519f512190d9a225258f855ebec4
Navid <navidraeiszadeh@gmail.com>
2022-06-30 16:09:21 +0430

    fixed ~

--- lib/post.g.dart
+++ lib/post.g.dart
@@ -7,29 +7,29 @@ part of 'post.dart';
 // **************************************************************************
 
 Post _$PostFromJson(Map<String, dynamic> json) => Post(
-  json['title'] as String,
-  json['description'] as String,
-  DateTime.parse(json['time'] as String),
-  json['userName'] as String,
-  json['community'] as String,
-  (json['comments'] as List<dynamic>)
-      .map((e) => Comment.fromJson(e as Map<String, dynamic>))
-      .toList(),
-)
-  ..likes = json['likes'] as int
-  ..isLiked = json['isLiked'] as bool
-  ..isDisLiked = json['isDisLiked'] as bool
-  ..json = json['json'] as String;
+      json['title'] as String,
+      json['description'] as String,
+      DateTime.parse(json['time'] as String),
+      json['userName'] as String,
+      json['community'] as String,
+      (json['comments'] as List<dynamic>)
+          .map((e) => Comment.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    )
+      ..likes = json['likes'] as int
+      ..isLiked = json['isLiked'] as bool
+      ..isDisLiked = json['isDisLiked'] as bool
+      ..json = json['json'] as String;
 
 Map<String, dynamic> _$PostToJson(Post instance) => <String, dynamic>{
-  'title': instance.title,
-  'description': instance.description,
-  'userName': instance.userName,
-  'community': instance.community,
-  'likes': instance.likes,
-  'comments': instance.comments,
-  'time': instance.time.toIso8601String(),
-  'isLiked': instance.isLiked,
-  'isDisLiked': instance.isDisLiked,
-  'json': instance.json,
-};
+      'title': instance.title,
+      'description': instance.description,
+      'userName': instance.userName,
+      'community': instance.community,
+      'likes': instance.likes,
+      'comments': instance.comments,
+      'time': instance.time.toIso8601String(),
+      'isLiked': instance.isLiked,
+      'isDisLiked': instance.isDisLiked,
+      'json': instance.json,
+    };
