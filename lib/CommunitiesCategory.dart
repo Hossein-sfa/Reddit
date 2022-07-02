80e7d251c5f1d737e44c77cdf8d1995fdeeb6896
Navid <navidraeiszadeh@gmail.com>
2022-07-02 07:17:47 +0430

    CommunitiesCategory.dart updated

--- lib/CommunitiesCategory.dart
+++ lib/CommunitiesCategory.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class ShowCommunitiesCategory extends StatefulWidget {
@@ -9,42 +11,80 @@ class ShowCommunitiesCategory extends StatefulWidget {
 }
 
 class _StateCommunitiesCategory extends State<ShowCommunitiesCategory> {
+
+  Future<String> getCommunity() async {
+    await Socket.connect("192.168.1.34", 8080).then((serverSocket) {
+      serverSocket.write(
+          'getcommunities~${title}\u0000');
+      serverSocket.flush();
+      serverSocket.listen((socket) {
+        setState(() {
+          comustr = String.fromCharCodes(socket);
+        });
+      });
+    });
+    return comustr;
+  }
+
+  void splitCommunities(){
+    var split = comustr.split("\n");
+    count = split.length ;
+    for (int i= 0 ; i<split.length ; i++) {
+      var split2 = split[i].split("~");
+      lst.add(split2[0]);
+      lst.add(split2[2]);
+      lst.add(split2[3]);
+    }
+  }
+  int count = 0 ;
+  List <String> lst = [];
   int catNum = 0;
   _StateCommunitiesCategory(this.catNum);
+  String comustr = "";
   String name = "";
+  String compic = "" ;
   String title = "";
   void _setter() {
     switch (catNum) {
       case 1:
         name = "assets/images/sociaty.jpg";
+        compic = name ;
         title = "Society & Life";
         break;
       case 2:
-        name = "assets/images/code.jpg";
+        name = "assets/images/technology.jpg";
+        compic = "assets/images/code.jpg";
         title = "Technology";
         break;
       case 3:
         name = "assets/images/nature.jpg";
+        compic = "assets/images/lion.jpg";
         title = "Nature & Animals";
         break;
       case 4:
         name = "assets/images/science_main.jpg";
+        compic = "assets/images/Science.jpg";
         title = "Science";
         break;
       case 5:
         name = "assets/images/Sp.jpg";
+        compic = "assets/images/sport.png";
         title = "Sports";
         break;
       case 6:
         name = "assets/images/movie.jpg";
+        compic = "assets/images/movie.jpg";
         title = "Entertainment";
         break;
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
     _setter();
+    getCommunity();
+    splitCommunities();
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white38,
@@ -124,6 +164,90 @@ class _StateCommunitiesCategory extends State<ShowCommunitiesCategory> {
                     ),
                   ],
                 ),
+              ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: count,
+
+                itemBuilder: (BuildContext context, int index) {
+                  return ListView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    // prevent to scroll it
+                    shrinkWrap: true,
+                    // we can scroll it just in list area
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 15),
+                        //Border:  BorderRadius.all(Radius.circular(15)),
+                        decoration: const BoxDecoration(
+                          color: Colors.white12,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 80,
+                              height: 80,
+                              child: ClipRRect(
+                                borderRadius:
+                                const BorderRadius.all(Radius.circular(15)),
+                                child: Image.asset(
+                                  compic,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    children: const [
+                                      Icon(Icons.person_outline_rounded,
+                                          color: Colors.white60, size: 17),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      Text(
+                                        " r/ nasa",
+                                        style: TextStyle(
+                                          color: Colors.white60,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const Text(
+                                    "Title",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Text(
+                                    "adad adwdwdd wdaedf frsfaea ffasa edada dsadjhdj jjjdjjsjdoad nsdajndjkdj sadsad dsad ",
+                                    overflow: TextOverflow.ellipsis,
+                                    //ToDo =  > implement it with short text of each communities
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                // prevent to scroll it
               ),
             ],
           ),
